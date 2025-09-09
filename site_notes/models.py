12cc05b8fb0f6374dd664bb16f61b6e66c12cb84
Aleksandr Zapolskiy <zapolskiyalex3007@gmail.com>
2025-09-09 20:11:42 +0300

    Создание моделей Sections и Chapters * Созданы модели Chapters и Sections для организации контента * Настроены связи между моделями (ForeignKey) по полю section * Выполнены миграции для создания таблиц в БД

--- site_notes/models.py
+++ site_notes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Sections(models.Model):
+    name = models.CharField(max_length=30, )
+    slug = models.SlugField(max_length=15, db_index=True, unique=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Chapters(models.Model):
+    name = models.CharField(max_length=30, unique=True, null=False)
+    slug = models.SlugField(max_length=15, unique=True, db_index=True)
+    text = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    section = models.ForeignKey('Sections', on_delete=models.PROTECT, related_name='chapter')
+    
+    def __str__(self):
+        return self.title
